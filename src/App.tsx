42f386c604cc9dc414c32822d2ba54f3e74e52ba
samiunarno <samiunarno.iub@gmail.com>
2025-07-25 11:16:25 +0600

    new push

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+
 import ChatInterface from './components/ChatInterface';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="min-h-screen bg-gray-100">
       <Header />
       <div className="flex h-[calc(100vh-80px)]">
-        <Sidebar />
+        
         <main className="flex-1 p-6">
           <div className="h-full max-w-4xl mx-auto">
             <ChatInterface />
